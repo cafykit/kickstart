d04519fcf2ea289af82854af2dcce0c38515d873
Amrendra Kumar <kuamrend@cisco.com>
2019-05-13 15:21:19 -0700

    Add Xr Operations AP

--- src/start/xr_operations_ap.py
+++ src/start/xr_operations_ap.py
@@ -9,16 +9,31 @@ class Test:
 
 class TestTopology:
     def test_connect(self,):
+        """
+        Connect to testbeds
+
+        :return:
+        """
         Test.device = Test.topo.get_device("R1")
         Test.device.connect()
         Test.peer =  Test.topo.get_device("R2")
         Test.peer.connect()
 
     def test_execute(self,):
+        """
+        Execute some sample commands on the testbed
+
+        :return:
+        """
         Test.device.execute("show version")
         Test.device.execute("show lldp neighbors")
 
     def test_config(self,):
+        """
+        Configure a simple bundle interface
+
+        :return:
+        """
         config = """
         interface Bundle-Ether738
             no shut
@@ -27,10 +42,20 @@ class TestTopology:
         Test.device.config("lldp")
 
     def test_parse(self,):
+        """
+        Configure lldp and Parse it
+
+        :return:
+        """
         Test.peer.config("lldp")
         Test.device.verify("show lldp neighbors",parse_only=True)
 
     def test_disconnect(self):
+        """
+        Disconnect from Device
+
+        :return:
+        """
         Test.device.disconnect()
         Test.peer.disconnect()
 
